93c358833504a1d1a6774e68c76caac70414036d
Nishoo Gupta <Nishoo.Gupta@ril.com>
2022-09-10 16:45:14 +0530

    resume uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Profile from './components/Profile';
+import LoadingBar from 'react-top-loading-bar';
+import React, {useState} from 'react'
 
 function App() {
+  const [progress, setProgress] = useState();
+  function setProgressBar(progress) {
+    setProgress(progress);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <LoadingBar
+      height={3}
+      color='#f11946'
+      progress={progress}
+    />
+    <div className="container">
+      <h2 className='text-center'>Thanks for taking a moment to read my resume</h2>
+      <div className="card-group">
+        <Sidebar setProgressBar = {setProgressBar}/>
+        <Profile setProgressBar = {setProgressBar}/>
+      </div>
     </div>
+    </>
   );
 }
 
